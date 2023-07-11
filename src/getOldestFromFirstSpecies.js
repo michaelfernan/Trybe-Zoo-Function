21291b4ddf218d472c5cbf5c8fe771a014326caf
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 18:03:29 -0300

    requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,16 @@
 const data = require('../data/zoo_data');
 
 const getOldestFromFirstSpecies = (id) => {
-  // seu código aqui
+  const employeeData = data.employees.find((employee) => employee.id === id);
+  const firstSpeciesId = employeeData.responsibleFor[0];
+  const species = data.species.find((specie) => specie.id === firstSpeciesId);
+  const oldestAnimal = species.residents.reduce((oldest, current) => {
+    if (current.age > oldest.age) {
+      return current;
+    }
+    return oldest;
+  });
+  return [oldestAnimal.name, oldestAnimal.sex, oldestAnimal.age];
 };
 
 module.exports = getOldestFromFirstSpecies;
