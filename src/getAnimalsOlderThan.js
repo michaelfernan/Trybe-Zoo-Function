acb882e818c65205c42c462ef47a39d2f996560f
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 13:17:43 -0300

    Requisito 2

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,7 +1,13 @@
 const data = require('../data/zoo_data');
 
 const getAnimalsOlderThan = (animal, age) => {
-  // seu código aqui
+  const species = data.species.find((species) => species.name === animal);
+
+  if (!species) {
+    throw new Error('Espécie não encontrada');
+  }
+
+  return species.residents.every((resident) => resident.age >= age);
 };
 
 module.exports = getAnimalsOlderThan;
