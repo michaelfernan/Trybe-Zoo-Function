7a256cf242686b899b1f1305e8adc6259e6cd212
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 17:45:24 -0300

    Requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,36 @@
 const data = require('../data/zoo_data');
 
 const countEntrants = (entrants) => {
-  // seu código aqui
+  const count = { child: 0, adult: 0, senior: 0 };
+
+  entrants.forEach((entrant) => {
+    if (entrant.age < 18) {
+      count.child += 1;
+    } else if (entrant.age >= 18 && entrant.age < 50) {
+      count.adult += 1;
+    } else {
+      count.senior += 1;
+    }
+  });
+
+  return count;
 };
 
 const calculateEntry = (entrants) => {
-  // seu código aquioi
+  if (!entrants || entrants.length === 0) {
+    return 0;
+  }
+
+  const entrantCount = countEntrants(entrants);
+  const { prices } = data;
+
+  const childPrice = prices.child * entrantCount.child;
+  const adultPrice = prices.adult * entrantCount.adult;
+  const seniorPrice = prices.senior * entrantCount.senior;
+
+  const total = childPrice + adultPrice + seniorPrice;
+
+  return parseFloat(total.toFixed(2));
 };
 
 module.exports = { calculateEntry, countEntrants };
