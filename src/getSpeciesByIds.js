63c3dc24b8d728f6ab7e1da41bea116039976cc6
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 13:30:13 -0300

    Requisito 1

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,5 +1,5 @@
 const data = require('../data/zoo_data');
-// os tres pontos(...) e conhecido como operador rest , ele nos permitem a receber um numero de varial de argumentos e agrupa-los em um array ,
+// Os tres pontos(...) e conhecido como operador rest , ele nos permitem a receber um numero de varial de argumentos e agrupa-los em um array ,
 // Nesse caso, o operador rest está sendo utilizado para receber múltiplos IDs como argumentos para a função getSpeciesByIds.
 const getSpeciesByIds = (...ids) => {
   if (!ids || ids.length === 0) {
