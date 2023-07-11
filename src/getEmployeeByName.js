a3c36ec234ea423f828e3ebeb975cbf772bdb1e1
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 13:38:26 -0300

    Requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
 
 const getEmployeeByName = (employeeName) => {
-  // seu código aqui
+  if (!employeeName) {
+    return {};
+  }
+
+  const employee = data.employees.find(
+    (emp) =>
+      emp.firstName === employeeName || emp.lastName === employeeName,
+  );
+
+  if (!employee) {
+    return {};
+  }
+
+  return {
+    id: employee.id,
+    firstName: employee.firstName,
+    lastName: employee.lastName,
+    managers: employee.managers,
+    responsibleFor: employee.responsibleFor,
+  };
 };
 
 module.exports = getEmployeeByName;
