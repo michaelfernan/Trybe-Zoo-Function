b9e7fe41eeddf9bb20730e54ddec0932c0460e9d
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 14:49:02 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,27 @@
 const data = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  // seu código aqui
+  const { species } = data;
+  const result = {};
+
+  if (!animal) {
+    for (const animal of species) {
+      result[animal.name] = animal.residents.length;
+    }
+  } else if (animal.species && !animal.sex) {
+    const foundSpecies = species.find((a) => a.name === animal.species);
+    result[foundSpecies.name] = foundSpecies.residents.length;
+  } else if (animal.species && animal.sex) {
+    const foundSpecies = species.find((a) => a.name === animal.species);
+    const filteredResidents = foundSpecies.residents.filter((resident) => resident.sex === animal.sex);
+    result[foundSpecies.name] = filteredResidents.length;
+  }
+
+  if (Object.keys(result).length === 1) {
+    return Object.values(result)[0];
+  }
+
+  return result;
 };
 
 module.exports = countAnimals;
