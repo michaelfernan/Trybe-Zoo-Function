3f2de9410f9b5af8353d2ceb603ad027181656a4
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-11 14:52:48 -0300

    requisito 6

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,48 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('handlerElephants', () => {
+  it('deve retornar a quantidade de elefantes', () => {
+    const result = handlerElephants('count');
+    expect(result).toBe(4);
+  });
+
+  it('deve retornar um array com os nomes dos elefantes', () => {
+    const result = handlerElephants('names');
+    expect(result).toContain('Jefferson');
+  });
+
+  it('deve retornar a média de idade dos elefantes', () => {
+    const result = handlerElephants('averageAge');
+    expect(result).toBeCloseTo(10.5, 1);
+  });
+
+  it('deve retornar a localização dos elefantes', () => {
+    const result = handlerElephants('location');
+    expect(result).toBe('NW');
+  });
+
+  it('deve retornar a popularidade dos elefantes', () => {
+    const result = handlerElephants('popularity');
+    expect(result).toBe(5);
+  });
+
+  it('deve retornar um array com os dias de disponibilidade dos elefantes', () => {
+    const result = handlerElephants('availability');
+    expect(result).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  it('deve retornar undefined se nenhum parâmetro for fornecido', () => {
+    const result = handlerElephants();
+    expect(result).toBeUndefined();
+  });
+
+  it('deve retornar uma mensagem de erro se o parâmetro não for uma string', () => {
+    const result = handlerElephants(123);
+    expect(result).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('deve retornar null se o parâmetro fornecido for inválido', () => {
+    const result = handlerElephants('invalidParam');
+    expect(result).toBeNull();
+  });
+});
