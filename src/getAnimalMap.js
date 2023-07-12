1ad238f611edd895a68439ee8aa50960147701ba
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-12 14:28:54 -0300

    Requisito 14

--- src/getAnimalMap.js
+++ src/getAnimalMap.js
@@ -1,7 +1,26 @@
 const data = require('../data/zoo_data');
+// codigo realizados com consutas na internet , e auxilio da desenvolvedora Michelle Fernandes e Dannille Jeanine
+function getAnimalMap(options) {
+  const { includeNames, sorted, sex } = options || {};
+  const animalMap = {
+    NE: [],
+    NW: [],
+    SE: [],
+    SW: [],
+  }; function filterBySex(residents) {
+    return !sex ? residents : residents.filter(({ sex: residentSex }) => residentSex === sex);
+  }
+  function sortNames(names) {
+    return sorted ? names.sort() : names;
+  }
+  data.species.forEach(({ name: animalName, location, residents }) => {
+    const filteredResidents = filterBySex(residents);
+    const names = includeNames ? sortNames(filteredResidents.map(({ name }) => name)) : undefined;
 
-const getAnimalMap = (options) => {
-  // seu código aqui
-};
+    animalMap[location].push(includeNames ? { [animalName]: names } : animalName);
+  });
+
+  return animalMap;
+}
 
 module.exports = getAnimalMap;
