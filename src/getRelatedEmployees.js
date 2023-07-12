d410cf149e0e50f64fc7a9e60683cb1e89036adb
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-12 14:37:13 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,22 @@
 const data = require('../data/zoo_data');
 
 const isManager = (id) => {
-  // seu código aqui
+  const { employees } = data;
+  return employees.some((employee) => employee.managers.includes(id));
 };
 
 const getRelatedEmployees = (managerId) => {
-  // seu código aqui
+  const { employees } = data;
+
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+
+  const managedEmployees = employees
+    .filter((employee) => employee.managers.includes(managerId))
+    .map((employee) => `${employee.firstName} ${employee.lastName}`);
+
+  return managedEmployees;
 };
 
 module.exports = { isManager, getRelatedEmployees };
