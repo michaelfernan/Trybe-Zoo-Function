5bfb31d3a627fe84d7787a01d85f39b0bc26913e
Michael Fernandes <111091339+michaelfernan@users.noreply.github.com>
2023-07-13 14:45:18 -0300

    Requisito 11

--- src/getEmployeesCoverage.js
+++ src/getEmployeesCoverage.js
@@ -1,7 +1,32 @@
 const data = require('../data/zoo_data');
+// Codigo realaizado com auxilo da desemvolvedora Michelle Fernando , juntos com pesquisa a internet.
 
-const getEmployeesCoverage = () => {
-  // seu código aqui
-};
+const employeesList = data.employees.map((objeto) => {
+  const fullName = `${objeto.firstName} ${objeto.lastName}`;
+  const species = objeto.responsibleFor.map((id) =>
+    data.species.find((specie) => specie.id === id));
+  const locations = species.map((specie) => specie.location);
+  return {
+    id: objeto.id,
+    fullName,
+    species: species.map((specie) => specie.name),
+    locations,
+  };
+});
+
+function getEmployeesCoverage(objeto) {
+  if (!objeto) {
+    return employeesList;
+  }
+  const findEmployee = employeesList.find(
+    (person) =>
+      person.fullName.includes(objeto.name) || person.id === objeto.id,
+  );
+
+  if (!findEmployee) {
+    throw new Error('Informações inválidas');
+  }
+  return findEmployee;
+}
 
 module.exports = getEmployeesCoverage;
